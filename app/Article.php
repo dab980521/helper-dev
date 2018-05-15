07d739ae78ff9aa85c76951884cc36dc6fe9879a
pdeantihuman <pdeantihuman@gmail.com>
2018-05-15 21:50:32 +0800

    create parent id attribute

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
+    public function getParentIdAttribute(){
+        return User::where('leftChild',$this->id)
+            ->orWhere('rightChild',$this->id)
+            ->get();
     }
 }
