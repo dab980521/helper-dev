fc8201d37761e9136daf2e0c6887600dcf1a2990
pdeantihuman <pdeantihuman@gmail.com>
2018-05-15 21:59:56 +0800

    add index method

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
+use App\Http\Resources\ArticleResource;
 use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
 {
-    //
+    public function index(Request $request){
+        $onlyRoot = $request->input("onlyRoot");
+        $model = Article::getModel();
+        if ($onlyRoot){
+            $model->where('isRoot',true);
+        }
+        return ArticleResource::collection($model->get());
+    }
 }
