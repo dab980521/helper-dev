254a077672c2730d0ff3e05dd65bb18ad0a18871
pdeantihuman <pdeantihuman@gmail.com>
2018-05-16 14:49:24 +0800

    write a basic test for article.index

---
+++ tests/Unit/Resources/ArticleTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit\Resources;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ArticleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testIndex(){
+        $response = $this->get(route('articles.index'));
+
+        $response->assertStatus(200);
+    }
+}
