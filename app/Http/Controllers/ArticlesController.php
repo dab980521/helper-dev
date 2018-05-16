f0386c6900dd1143576e2d5b71c0a2285259836b
pdeantihuman <pdeantihuman@gmail.com>
2018-05-16 15:16:12 +0800

    implement tree method and index method

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
 {
+    private $box;
+
     public function index(Request $request){
         $onlyRoot = $request->input("onlyRoot");
         $model = Article::getModel();
@@ -15,5 +17,37 @@ class ArticlesController extends Controller
             $model->where('isRoot',true);
         }
         return ArticleResource::collection($model->get());
+    }
+
+    public function show($id){
+        return "TODO:".$id ;
+    }
+
+    public function tree($id) {
+        // 清除缓存
+        $this->box = collect();
+        // 查找 root
+        $root = Article::findOrFail($id)->root;
+        // 获取树
+        $articles= Article::where('root',$root)->get();
+        // 将 Database Collection 改造成 ( $id => $item ) Hash Map
+        $this->box = $articles->mapWithKeys(function($item){
+            return [ $item->id => $item];
+        });
+        return $this->treeify($root);
+    }
+
+    private function treeify($node){
+        $children = collect();
+        if ($this->box[$node]->leftChild){
+            $children->push($this->treeify($this->box[$node]->leftChild));
+        }
+        if ($this->box[$node]->rightChild){
+            $children->push($this->treeify($this->box[$node]->rightChild));
+        }
+        return [
+            "title" => $this->box[$node]->title,
+            "children" => $children
+        ];
     }
 }
