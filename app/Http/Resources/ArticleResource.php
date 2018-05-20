2423b37f1b202dd900a009c5ce1a0bc4a412e8fa
pdeantihuman <pdeantihuman@gmail.com>
2018-05-20 20:19:09 +0800

    add id field to Article jsonify

--- app/Http/Resources/ArticleResource.php
+++ app/Http/Resources/ArticleResource.php
@@ -15,7 +15,8 @@ class ArticleResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'title' => $this->title
+            'title' => $this->title,
+            'id' => $this->id
         ];
     }
 }
