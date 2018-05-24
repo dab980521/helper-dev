da9e7e595d6bcee5c4fd154e30529f876b47ef25
pdeantihuman <pdeantihuman@gmail.com>
2018-05-24 11:26:47 +0800

    feat: write api auth middleware

---
+++ app/Http/Middleware/ApiAuthentication.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Cache;
+
+class ApiAuthentication
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $api_token = Cache::get($request->name);
+        if ($request->_token != $api_token){
+            return response()->json([
+                'message' => '权限验证错误'
+            ]);
+        }
+
+        return $next($request);
+    }
+}
